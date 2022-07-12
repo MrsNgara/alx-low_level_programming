81496ac31aa262c6f9a02274440a341679e01dd4
MrsNgara <blessingzuze95@gmail.com>
2022-07-12 07:33:38 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value of n to 98
- * @n: integer pointer to a variable to be updated
- *
+ * reset_to_98 - Takes a pointer to an int and updates
+ *  the value it points to 98.
+ *  @n: The pointer to an int.
  * Return: nothing
  */
 
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#endif
+
+/**
+ * File: Main.h
+ * Desc: Header file containing prototypes
+ * used in directory.
+ */
+
+#include <stdlib.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
